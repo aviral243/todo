afe62a08d3daea6dd178af4dba2002979149b6cb
aviral243 <aviralgangwar24@gmail.com>
2019-12-23 22:59:25 +0530

    update task component

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
 
+import { Tasks } from "../api/tasks";
+
 // Task component - represents a single todo item
 export default class Task extends Component {
+  toggleChecked() {
+    Tasks.update(this.props.task._id, {
+      $set: { checked: !this.props.task.checked }
+    });
+  }
+
+  deleteThisTask = () => {
+    Tasks.remove(this.props.task._id);
+  };
   render() {
-    return <li>{this.props.task.text}</li>;
+    const taskClassName = this.props.task.checked ? "checked" : "";
+
+    return (
+      <li>
+        {this.props.task.text}
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+          &times;
+        </button>
+        <input
+          type="checkbox"
+          readOnly
+          checked={!!this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+        <span className="text">{this.props.task.text}</span>
+      </li>
+    );
   }
 }
