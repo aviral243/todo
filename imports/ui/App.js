5e7758aade9bb1208e52e42941adf444f5b7101b
aviral243 <aviralgangwar24@gmail.com>
2019-12-23 22:47:17 +0530

    form for adding tasks

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import { withTracker } from "meteor/react-meteor-data";
 
 import { Tasks } from "../api/tasks";
@@ -7,6 +8,18 @@ import Task from "./Task.js";
 
 // App component - represents the whole app
 class App extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date()
+    });
+
+    ReactDOM.findDOMNode(this.refs.textInput).value = "";
+  }
   renderTasks() {
     return this.props.tasks.map(task => <Task key={task._id} task={task} />);
   }
@@ -16,6 +29,13 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
         </header>
 
         <ul>{this.renderTasks()}</ul>
