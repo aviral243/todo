271d6bb08f1c4efbcea115127776e7126af5a758
aviral243 <aviralgangwar24@gmail.com>
2019-12-23 23:19:23 +0530

    add hide completed in state

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -8,6 +8,13 @@ import Task from "./Task.js";
 
 // App component - represents the whole app
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hideCompleted: false
+    };
+  }
+
   handleSubmit(event) {
     event.preventDefault();
 
@@ -20,6 +27,13 @@ class App extends Component {
 
     ReactDOM.findDOMNode(this.refs.textInput).value = "";
   }
+
+  toggleHideCompleted = () => {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted
+    });
+  };
+
   renderTasks() {
     return this.props.tasks.map(task => <Task key={task._id} task={task} />);
   }
@@ -29,6 +43,15 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
             <input
               type="text"
