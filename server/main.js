b5026e072fbe6c53906ef52b32c606baee84975f
aviral243 <aviralgangwar24@gmail.com>
2019-12-24 00:05:14 +0530

    fixed a bug

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 import { Meteor } from "meteor/meteor";
 import "../imports/api/tasks";
-import "../imports/startup/accounts-config";
+// import "../imports/startup/accounts-config";
 
 Meteor.startup(() => {
   // code to run on server at startup
